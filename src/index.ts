4cee2ee9ad86d87c5bab0e72d16db76fb4825d10
Julia Bubnova <bubnova.j.i@gmail.com>
2023-01-12 22:01:52 +0100

    Init dummy telegram bot, read token from .env

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello World');
+const TelegramBot = require('node-telegram-bot-api');
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+const token = process.env.TELEGRAM_TOKEN;
+console.log('Telegram bot token: ', token);
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', (msg) => {
+
+  const hi = "hi";
+  if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
+    bot.sendMessage(msg.chat.id,"Hello dear user");
+  }
+
+});
